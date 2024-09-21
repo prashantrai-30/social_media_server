f201064c8e0ffd8cf617dd0ee788fbf8e6398c54
prashantrai-30 <prashantkumar2488@gmail.com>
2024-09-21 22:54:36 +0530

    made changes in cors

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const cors = require('cors');
-app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:3000' }));
+app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI);
